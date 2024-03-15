a4da065da1bdba431cebf3452d0f637fd35023eb
Mosayeb09 <mosayebhossain09@gmail.com>
2024-03-15 15:29:12 +0600

    add evenhandler

--- src/Component/PostComponent/PostComponent.jsx
+++ src/Component/PostComponent/PostComponent.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const PostComponent = ({post}) => {
     const {id,title}=post;
@@ -8,11 +8,21 @@ const PostComponent = ({post}) => {
         borderRadius:'20px'
 
     }
+    const navigate = useNavigate();
+    const handleDetails = ()=>{
+        navigate(   `/post/${id}`)
+        
+    }
+    const handleGOBack = ()=>{
+        navigate(-1)
+    }
     return (
         <div style={postStyle}>
             <h2>post of id:{id}</h2>
             <p>post of title :{title}</p>
             <Link to={`/post/${id}`}>Post Details</Link>
+            <button onClick={handleDetails}>See Details</button>
+            <button onClick={handleGOBack}>Go Back</button>
         </div>
     );
 };
--- src/Component/postDetails/PostDetails.jsx
+++ src/Component/postDetails/PostDetails.jsx
@@ -2,9 +2,10 @@ import { useLoaderData } from "react-router-dom";
 
 const PostDetails = () => {
     const post =useLoaderData();
-    const {body} = post;
+    const {body,id} = post;
     return (
         <div>
+            <h2>POst details about :{id}</h2>
             <h2>Post Details: {body}</h2>
         </div>
     );
